43d1af8de2e1f78c396d38146697f1ecd52346e8
Nguyen Huu Trung <vfa.trungng@gmail.com>
2017-11-01 13:44:20 +0700

    20171101:1

---
+++ deploy_dev.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ssh ec2-user@54.238.158.106 << EOF
+
+echo '1. Updating sources'
+#cd /patch/to/your/repository/
+#git checkout --force master
+#git pull
+
+#echo "2. Restart apache"
+#sudo apache2ctl graceful
+
+cd /var/www/html/
+mkdir checkCI
+echo 'Done!'
+
+EOF
