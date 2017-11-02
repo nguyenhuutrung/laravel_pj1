4b57c211a4741e1d91f5aa1a0d424b390b7750d5
Nguyen Huu Trung <vfa.trungng@gmail.com>
2017-11-02 17:31:50 +0700

    up dev check deploy

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -4,9 +4,9 @@ ssh -o "StrictHostKeyChecking no" ec2-user@54.238.158.106 << EOF
 
 echo '1. Updating sources'
 
-#cd /var/www/html/laravel_pj1
-#git pull
-#composer install -n --prefer-dist --ignore-platform-reqs
+cd /var/www/html/laravel_pj1
+git pull
+composer install -n --prefer-dist --ignore-platform-reqs
 
 echo '2.Done!!'
 
