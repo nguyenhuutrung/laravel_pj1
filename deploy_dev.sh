497e8d168e03d95ed7f9851b3831fdaa98a37c1d
Nguyen Huu Trung <vfa.trungng@gmail.com>
2017-11-02 13:32:11 +0700

    update dev to run

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -3,15 +3,17 @@
 ssh -o "StrictHostKeyChecking no" ec2-user@54.238.158.106 << EOF
 
 echo '1. Updating sources'
-#cd /patch/to/your/repository/
-#git checkout --force master
-#git pull
+cd /var/www/html/laravel_pj1
+#git checkout --force dev
+git pull
+
+composer install -n --prefer-dist --ignore-platform-reqs
 
 #echo "2. Restart apache"
 #sudo apache2ctl graceful
 
-cd /var/www/html/
-mkdir checkCI_dev
+#cd /var/www/html/
+#mkdir checkCI_dev
 echo '2.Done!'
 
 EOF
