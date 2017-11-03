615bf91b76820d0dfe87f7aa20f22790715d7b63
Nguyen Huu Trung <vfa.trungng@gmail.com>
2017-11-03 10:41:01 +0700

    update dev 2

---
+++ deploy_staging.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ssh -o "StrictHostKeyChecking no" ec2-user@54.238.158.106 << EOF
+
+echo '1. Updating sources'
+
+cd /var/www/html/checkstaging/laravel_pj1
+git pull
+composer install -n --prefer-dist --ignore-platform-reqs
+
+echo '2.Done!!!'
+
+EOF
