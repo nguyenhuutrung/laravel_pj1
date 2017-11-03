e5c1851f802be052e7f4c12fe8fadd962f26c689
Nguyen Huu Trung <vfa.trungng@gmail.com>
2017-11-03 11:14:23 +0700

    update master 4

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-if [ "$1" = "master" ]; then
-	ssh -o "StrictHostKeyChecking no" ec2-user@54.238.158.106 << EOF
-elif [ "$1" = "staging" ]; then
-	ssh -o "StrictHostKeyChecking no" ec2-user@54.238.158.106 << EOF
-fi
+ssh -o "StrictHostKeyChecking no" ec2-user@54.238.158.106 << EOF
+# if [ "$1" = "master" ]; then
+# 	ssh -o "StrictHostKeyChecking no" ec2-user@54.238.158.106 << EOF
+# elif [ "$1" = "staging" ]; then
+# 	ssh -o "StrictHostKeyChecking no" ec2-user@54.238.158.106 << EOF
+# fi
 
 echo '1. Updating sources'
 
-if [ "$1" = "master" ]; then  
-	cd /var/www/html/laravel_pj1
-elif [ "$1" = "staging" ]; then
-	cd /var/www/html/checkstaging/laravel_pj1
-fi
+# if [ "$1" = "master" ]; then  
+# 	cd /var/www/html/laravel_pj1
+# elif [ "$1" = "staging" ]; then
+# 	cd /var/www/html/checkstaging/laravel_pj1
+# fi
+cd /var/www/html/laravel_pj1
 git pull
 composer install -n --prefer-dist --ignore-platform-reqs
 
