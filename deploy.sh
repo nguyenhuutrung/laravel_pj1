c8296185dae7402eeb02bef5be5b1ba703500fca
Nguyen Huu Trung <vn.nguyenhuutrung@gmail.com>
2017-11-07 15:53:24 +0700

    update master

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
+wget -qO- http://checkip.amazonaws.com
 
 ssh -o "StrictHostKeyChecking no" $2 << EOF
 
 echo '1. Updating sources'
 
-if [ $1 = "master" ]; then  
-	cd /var/www/html/laravel_pj1
-elif [ $1 = "staging" ]; then
-	cd /var/www/html/checkstaging/laravel_pj1
-fi
-git pull
-composer install -n --prefer-dist --ignore-platform-reqs
+# if [ $1 = "master" ]; then  
+# 	cd /var/www/html/laravel_pj1
+# elif [ $1 = "staging" ]; then
+# 	cd /var/www/html/checkstaging/laravel_pj1
+# fi
+# git pull
+# composer install -n --prefer-dist --ignore-platform-reqs
 
 echo '2.Done!!!'
 
 EOF
+
+wget -qO- http://checkip.amazonaws.com
